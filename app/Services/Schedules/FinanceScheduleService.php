b90f40a53e7f7aaf2cf44ebd3f99ae75fb61428a
Gustavo Alejandro <llxsantaellaxll@gmail.com>
2023-06-25 09:51:08 -0400

    dev (#59)
    * fix: downgrade to laravel 9 and configure somethings
    
    * feat: create req shotcut
    
    * feat: add docker files and config nginx
    
    * fix: composer install in dockerfile
    
    * fix: composer intall
    
    * fix: composer install
    
    * fix: docketfile v3
    
    * fix: composer dockerfile v4
    
    * fix: composer
    
    * fix: dockerfile
    
    * run app
    
    * fix: dockerfile
    
    * fix: dockerfile
    
    * fix: permissions file
    
    * install bash
    
    * fix: bash
    
    * fix: remove apt
    
    * fix: CMD
    
    * fix: directory
    
    * fix: comment chmod
    
    * fix: dockerfile and health endpoint
    
    * fix: dockerfile
    
    * fix: remove current directory
    
    * fix: dockerfile
    
    * fix: add ls
    
    * dockerfile
    
    * fix: command
    
    * remove &&
    
    * remove serve
    
    * fix: remove chmod
    
    * fix: string cmd
    
    * fix: remove CMD
    
    * add serve command to CMD
    
    * fix: server to serve
    
    * fix: php
    
    * fix: array commands
    
    * fix: start.sh CMD
    
    * fix: serve command
    
    * fix: php
    
    * cmd
    
    * fix: RUN
    
    * fix: cmd v2
    
    * fix: +rx
    
    * chmod 777
    
    * fix: add host
    
    * advances
    
    * fix: objectdi - in progress
    
    * fix: laravel-api
    
    * feat: get all countries (names)
    
    * fix:
    
    * Update README.md
    
    * fix: middleware
    
    * feat: balance in progress
    
    * fix: remove expired to jwt
    
    * fix: advances
    
    * fix: wallet advances
    
    * feat: history
    
    * feat: advances
    
    * fix: advance
    
    * fix: number_format
    
    * feat: history dtal
    
    * feat: delete
    
    * fet: finish migrate
    
    * feat: add log
    
    * feat: add url to logs and logout
    
    * feat: number format
    
    * feat: reset password
    
    * reset password
    
    * fix: add validation
    
    * fix: add country to user
    
    * feat: schedules
    
    * fix: update nextdate
    
    * fix: change to daily
    
    * feat: delete movement
    
    * laravel api (#35)
    
    * feat: add privacy and policies
    
    * feat: more logs
    
    * fix: add email validation
    
    * Restore Finances (#43)
    
    * feat: add privacy and policies
    
    * feat: more logs
    
    * fix: add email validation
    
    * Create restore finance (#41)
    
    Feature added.
    
    ---------
    
    Co-authored-by: Gustavo Santaella <llxsantaellaxll@gmail.com>
    
    * fix: structure (#45)
    
    * issue-47 (#48)
    
    * raname method
    
    * fix: fix namespace
    
    * fix: namepsace
    
    * fix: change folders
    
    * fix: uppercase
    
    ---------
    
    Co-authored-by: Luis Gutierrez <85537986+LR-Gutierrez@users.noreply.github.com>

---
+++ app/Services/Schedules/FinanceScheduleService.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services\Schedules;
+
+use App\Helpers\Date;
+use App\Helpers\DateString;
+use App\Helpers\Log;
+use App\Repositories\Schedules\FinanceScheduleRepository;
+use App\Services\Service;
+// fixed
+use App\Services\Wallet\WalletService;
+use Exception;
+use MongoDB\Operation\Aggregate;
+
+class FinanceScheduleService extends Service
+{
+    public function __construct(
+        private FinanceScheduleRepository $financeScheduleRepository,
+        private WalletService $walletService
+        )
+    {
+    }
+
+    public function getByWallet($walletId){
+        try{
+
+            $data = $this->financeScheduleRepository->getByWallet($walletId);
+
+            return $data;
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    public  function create($data)
+    {
+        try {
+            $schedule = $this->financeScheduleRepository->findByName($data["name"]);
+            if($schedule){
+                throw new Exception("This schedule already exists");
+            }
+            $wallet = $this->walletService->getByPk($data['walletId']);
+            $scheduleTypes = config("definitions.schedules.finance.types");
+            $financeTypes = config("definitions.finance.types");
+
+            $contain = collect($scheduleTypes)->contains($data['periodicity']);
+            if (!$contain) {
+                throw new Exception("Invalid periodicity");
+            }
+
+            $contain = collect($financeTypes)->contains($data['type']);
+            if (!$contain) {
+                throw new Exception("Invalid type");
+            }
+
+            return $this->financeScheduleRepository->create([
+                "name" => $data["name"],
+                "amount" => (float) $data["amount"],
+                "type" => $data["type"],
+                "userId" => auth()->user()->_id,
+                "walletId" => $wallet->id,
+                "startDate" => DateString::now(),
+                "periodicity" => $data["periodicity"],
+                "categoryId" => $data["categoryId"],
+                "nextDate" => DateString::nexDate($data['periodicity']),
+            ]);
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function delete($scheduleId){
+        try{
+
+            $this->financeScheduleRepository->deleteByPk($scheduleId);
+            return true;
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+
+    public function allOfToday(){
+        try{
+            $today = DateString::now();
+            return $this->financeScheduleRepository->whereToday($today);
+        }catch(Exception $e){
+            throw $e;
+        }
+    }
+}
